07e4be12ab10f8322980509373681f53ec28913e
nasangw <good617boy@gmail.com>
2018-05-07 19:31:47 +0900

    Add, Edit - webpack에 scss 파일 컴파일 기능 추가, internal 스타일 코드를 scss파일로 이동.

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,54 @@
+const webpack = require('webpack');
 const path = require('path');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+// const ManifestPlugin = require('webpack-manifest-plugin');
 
 module.exports = {
-//   entry: './src/index.js',
-    mode: 'development',
-    devtool: '#inline-source-map',
     entry: {
-        app: './src/js/app.js',
-        venders: './src/js/vendors.js',
+        app: [
+            './src/js/app.js',
+            './src/sass/app.scss',
+        ]
+        // js_lib: ["jquery", "bootstrap", "underscore"],
     },
     output: {
         filename: '[name].js',
-        path: path.resolve(__dirname, 'dist/js'),
-        chunkFilename: '[id].[chunkhash].js'
-    }
-};
+        path: path.join(__dirname, './dist/js')
+    },
+    mode: 'development',
+    // devtool: '#inline-source-map',
+    devtool: "cheap-module-eval-source-map",
+    resolve: {
+        modules: ["./node_modules"]
+    },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: [ 'css-loader', 'sass-loader' ]
+                })
+            },
+        ]
+    },
+    plugins: [
+        new ExtractTextPlugin('../css/[name].css'),
+        // new webpack.ProvidePlugin({
+        //     $: "jquery",
+        //     jQuery: "jquery"
+        // }),
+        // new ManifestPlugin(),
+        new UglifyJSPlugin(),
+        // new webpack.optimize.CommonsChunkPlugin({
+        //     name: 'js_lib',
+        //     minChunks: function (module) {
+        //         return module.context && module.context.indexOf('node_modules') !== -1;
+        //     }
+        // }),
+        // new webpack.optimize.CommonsChunkPlugin({
+        //     name: 'manifest'
+        // }),
+    ]
+}
