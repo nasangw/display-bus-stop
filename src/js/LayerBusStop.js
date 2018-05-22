8c28b6025571c302c9304a6404849067c451d930
nasangw <good617boy@gmail.com>
2018-05-22 16:07:19 +0900

    Edit - 버스루트 값에 대한 switch설정 추가

--- src/js/LayerBusStop.js
+++ src/js/LayerBusStop.js
@@ -4,7 +4,6 @@ export default class LayerBusStop {
     constructor(list) {
         this.overlayBusStop = {};
         this.listBusStop = list;
-        console.log(this.listBusStop);
     }
 
     getTemplateLayer(stop) {
@@ -20,7 +19,7 @@ export default class LayerBusStop {
                 </div>
                 <p>stationId: ${stop.stationId}</p>
                 <p>arsId: ${stop.arsId}</p>
-                <ul id="stopId-${stop.stationId}" class="list"></ul>
+                <ul id="stop-${stop.arsId}" class="list-arrival"></ul>
             </div>
         `;
     }
@@ -46,8 +45,7 @@ export default class LayerBusStop {
             .then(data => {
                 this.xml2js = new config.x2js();
                 const parseData = this.xml2js.xml_str2json(data);
-                resolve(parseData);
-                // resolve(data);
+                resolve(parseData.ServiceResult.msgBody);
             })
             .catch(err => {
                 reject(err);
@@ -62,13 +60,13 @@ export default class LayerBusStop {
 
         const { stop, map, marker } = data;
         const template = this.getTemplateLayer(stop);
+        const arsId = stop.arsId;
 
-        this.setOverlay({ template, map, marker, stationId: stop.stationId });
-        // this.ttt(stop.arsId);
+        this.setOverlay({ template, arsId, map, marker, stationId: stop.stationId });
     }
 
     setOverlay(data) {
-        const { template, map, marker, stationId } = data;
+        const { template, arsId, map, marker, stationId } = data;
 
         this.overlayBusStop[stationId] = new daum.maps.CustomOverlay({
             content: template, 
@@ -79,6 +77,9 @@ export default class LayerBusStop {
         // bind click event that show layer
         daum.maps.event.addListener(marker, 'click', function() {
             this.overlayBusStop[stationId].setMap(map);
+
+            // next step
+            this.setTemplateArrivalInfo(arsId);
         }.bind(this));
 
         const elemOverlay = this.overlayBusStop[stationId].a;
@@ -86,24 +87,84 @@ export default class LayerBusStop {
 
         // bind click event that hide layer
         btnCloseLayer.addEventListener('click', ev => {
+            // remove DOM for stop info
+            document.querySelector(`#stop-${arsId}`).innerHTML = '';
+
+            // hide overlay
             this.overlayBusStop[stationId].setMap(null);
         });
         // and hide layer
         this.overlayBusStop[stationId].setMap(null);
-
-        // next step
-        // this.ttt();
     }
 
-    ttt() {
-        this.getDataStop(stop.arsId)
-        .then(res => {
-            console.log(res);
-            const arrayBusStop = res.msgBody.itemList;
-            arrayBusStop.map((value, index) => {
-                console.log(value);
-                console.log(index);
-            })
+    getSwitchRouteType(data) {
+        if(!data) {
+            return;
+        }else {
+            if(typeof data !== 'number') {
+                data = parseInt(data);
+            }
+        }
+
+        switch(data) {
+            case 1: 
+                return '공항';
+                break;
+            case 2: 
+                return '마을';
+                break;
+            case 3: 
+                return '간선';
+                break;
+            case 4: 
+                return '지선';
+                break;
+            case 5: 
+                return '순환';
+                break;
+            case 6: 
+                return '광역';
+                break;
+            case 7: 
+                return '인천';
+                break;
+            case 8: 
+                return '경기';
+                break;
+            case 9: 
+                return '폐지';
+                break;
+            case 0: 
+                return '공용';
+                break;
+            default: 
+                return;
+                break;
+        }
+    }
+
+    setTemplateArrivalInfo(arsId) {
+        this.getDataStop(arsId)
+        .then(data => {
+            const arrayArrivalInfo = data.itemList.length ? data.itemList : undefined;
+            const elem = document.querySelector(`#stop-${arsId}`);
+
+            if(arrayArrivalInfo) {
+                const rrr = `
+                    ${arrayArrivalInfo.map(info => `
+                        <li class="bus">
+                            <span class="bus__routeType">${this.getSwitchRouteType(info.routeType)}</span>
+                            <span class="bus__number">${info.rtNm}</span>
+                            <span class="bus__direction">(${info.adirection} 방면)</span>
+                            <span class="bus__message">${info.arrmsg1}</span>
+                        </li>
+                    `).join('')}
+                `;
+                elem.innerHTML = rrr;
+            }
+        })
+        .catch(err => {
+            return
         });
     }
 }
